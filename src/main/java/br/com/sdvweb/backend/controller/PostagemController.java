82d216c16f6890259649fb3836dfd9c94a3f2915
unknown <leandrofreitas.dev@gmail.com>
2025-06-23 15:34:42 -0300

    Atualizar .gitignore e ignorar arquivos do H2

--- src/main/java/br/com/sdvweb/backend/controller/PostagemController.java
+++ src/main/java/br/com/sdvweb/backend/controller/PostagemController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import br.com.sdvweb.backend.DTO.PostagemDTO;
 import br.com.sdvweb.backend.service.PostagemService;
 import jakarta.validation.Valid;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/postagem")
 public class PostagemController {
